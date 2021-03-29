12ea206870b8482caa2799650e48bdcdd90d3286
Oleg <oleg419583@yandex.ru>
2021-03-29 12:52:27 +0300

    new dir

---
+++ client/src/components/Cards.vue
@@ -0,0 +1,114 @@
+<template>
+<div>
+  <div class="fl" >
+    <div>
+      <ul>
+        <li
+            v-for="item in audiences"
+            v-bind:key="item.id"
+            class="item"
+            v-bind:class="{open: item.switch}"
+        >
+          <h2>{{item.tittle}}</h2>
+          <button
+              v-on:click="$emit('newRoom',item.id),
+               item.switch = !item.switch"
+          >
+            Посмотреть</button>
+        </li>
+      </ul>
+    </div>
+  </div>
+    <Table  v-on:setDelete="deleteMe"
+            v-bind:class="{active: isActive}"
+            v-bind:room="room"
+    />
+</div>
+</template>
+
+<script>
+
+import Table from "@/components/Table";
+export default {
+  props:['audiences','room'],
+name: "Cards",
+  data() {
+    return{
+      none:'none',
+      isActive:false,
+      isSwitch:false,
+    }
+  },
+  components:{
+    Table
+  },
+  methods:{
+    deleteMe(id){
+      this.$emit('deleteMe',id)
+    },
+  }
+}
+</script>
+
+<style scoped>
+*{
+  box-sizing: border-box;
+}
+.open {
+  position: absolute;
+  background: lightslategray;
+}
+.item {
+  min-width: 150px;
+  transition: left  0.5s ease-in-out , background 0.5s ease-in-out;
+}
+.item:hover {
+
+}
+
+.item:hover {
+
+}
+
+.none {
+  display: none;
+}
+.active {
+  display: table;
+}
+.fl {
+  display: flex;
+}
+ul{
+  position: relative;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+}
+li {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-content: center;
+  margin-left: 15px;
+  padding: 30px;
+  list-style: none;
+  background: #8a307f;
+  border-radius: 12px;
+}
+h2 {
+  text-align: center;
+  font-size: 18px;
+  color: wheat;
+}
+button {
+  font-family: "Lucida Sans Unicode", "Lucida Grande", Sans-Serif;
+  color: white;
+  text-shadow: 0 1px 1px #2d2020;
+  padding: 10px;
+  border: none;
+  background: #79a7d3;
+  border-radius: 7px;
+}
+
+</style>
---
+++ client/src/components/Table.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="wrapper">
+    <table>
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>CPU</th>
+          <th>Video</th>
+          <th>RAM</th>
+          <th>Кнопка</th>
+        </tr>
+      </thead>
+      <tbody >
+        <tr v-for="item in room" v-bind:key="item.id">
+          <td>{{item.id}}</td>
+          <td>{{item.tittle}}</td>
+          <td>{{item.Video}}</td>
+          <td>{{item.RAM}}</td>
+          <td><button class="del-btn" v-on:click="$emit('setDelete',item.id)">Удалить</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['room'],
+  name: "Table",
+  data() {
+    return{
+
+    }
+  },
+}
+</script>
+
+<style>
+.wrapper {
+  max-width: 700px;
+  margin: 0 auto;
+}
+.del-btn {
+  padding: 10px;
+  border-radius: 12px;
+  border: none;
+}
+.none {
+  display: none;
+}
+th:first-child {
+  border-top-left-radius: 10px;
+}
+th:last-child {
+  border-top-right-radius: 10px;
+}
+tr:last-child td:first-child {
+  border-radius: 0 0 0 10px;
+}
+tr:last-child td:last-child {
+  border-radius: 0 0 10px 0;
+}
+th {
+  color: white;
+  padding: 5px;
+  border-style: solid;
+  border-width: 0 1px 1px 0;
+  border-color: white;
+}
+th {
+  background: #8a307f;
+  text-shadow: 0 1px 1px #2d2020;
+  border-style: solid;
+  border-width: 0 1px 1px 0;
+  border-color: white;
+  text-align: center;
+  padding: 5px;
+}
+td {
+  border-style: solid;
+  border-width: 0 1px 1px 0;
+  border-color: white;
+  padding: 10px 20px;
+  background: #6883bc;
+}
+table {
+  font-family: "Lucida Sans Unicode", "Lucida Grande", Sans-Serif;
+  font-size: 14px;
+  border-radius: 10px;
+  border-spacing: 0;
+  text-align: center;
+  width: 100%;
+  /*border: 1px solid black;*/
+  /*border-collapse: collapse;*/
+}
+</style>
